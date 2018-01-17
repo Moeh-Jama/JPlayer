f16a5311e21e9e71bcebd503c89836d0d958bf66
Mohamed Jama <m.d.jama@hotmail.com>
2018-01-17 15:12:01 +0000

    Create Main.java
    Main control of the system.

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.util.ArrayList;
+
+
+public class Main {
+	static String Path = "";
+	
+	static ArrayList<Song> library = new ArrayList<Song>();
+	public static void main(String[] args) {
+		//Getting the File Path, Creating a library of the songs in Path (Folder).
+		Path = FileDirectories.getMusicFolderPath();
+		createLibrary(Path);
+		new JPlayerGUI();
+	}
+	private static void createLibrary(String Path) {
+		File musicFolder = new File(Path);
+		for(File files: musicFolder.listFiles()) {
+			Song song = new Song();
+			song.setTitle(getCleanedName(files.getName()));
+			song.setFavourite(false);
+			song.setLength(0);
+			song.setPath(Path+"\\"+files.getName());
+			library.add(song);
+		}
+	}
+	private static String getCleanedName(String Name) {
+		String revisedName = "";
+		String[] s = Name.split("");
+		for(int i=0; !s[i].equals("."); i++)
+		{
+			revisedName+=s[i];
+		}
+		return revisedName;
+	}
+	public static ArrayList<Song> returnLibrary(){
+		return library;
+	}
+	
+}
+
